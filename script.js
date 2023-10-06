adec2a77c5adf634e829bda9fd2be697da0fd2af
Tuan Anh <tunanguyenlc@gmail.com>
2023-10-06 15:11:30 +0700

    update-2

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ sTrigger.addEventListener('click', () => {
 // SLIDER
 const sliderThumb = new Swiper('.thumb-nav', {
     spaceBetween: 10,
-    slidesPerview: 3,
+    slidesPerView: 3,
     slidesPerGroup: false,
     breakpoints: {
         992: {
@@ -43,11 +43,33 @@ const sliderThumb = new Swiper('.thumb-nav', {
 });
 
 const theSlider = new Swiper('.thumb-big', {
-    slidePerview: 1,
+    slidesPerView: 1,
     pagination: {
         el: '.swiper-pagination',
     },
     thumbs: {
-        swiper: sliderThumb
+        swiper: sliderThumb,
     }
 });
+
+// TABBED PRODUCTS
+const tabbeNav = new Swiper('.tnav', {
+    spaceBetween: 20,
+    slidesPerView: 6,
+    centeredSlides: true,
+    slidesPerGroup: false,
+});
+
+const theTab = new Swiper('.tabbed-item', {
+    loop: true,
+    slidesPerView: 1,
+    thumbs: {
+        swiper: tabbeNav,
+    }
+});
+
+
+
+
+
+
